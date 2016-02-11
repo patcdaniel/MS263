aac83e642c7cc6d8bd91bceb9bd9130d5b2f0c6e
Patrick Daniel <patcdaniel@gmail.com>
2016-02-11 08:58:20 -0800

    Updated alpha values for confidence intervals for t test

--- hw1/z_score.py
+++ hw1/z_score.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Feb  6 10:37:27 2016
-@author: Patrick
+@author: Patrick (pdaniel@mlml.calstate.edu)
 """
 from numpy import asarray,mean,std,sqrt
 from scipy import stats
@@ -22,32 +22,33 @@ def confidence_t(dataArray,confidence=.95):
     dataArray -- Any list of int or float type
     confidence -- a decimal speciffying the confidence intervals (default - .95)
     """
+    
+    confidence = confidence + ((1 - confidence) / 2) # for a 95% confidence interval, 
+    # for stats.t.ppf() use .975 for q for 95% confidence
+    # Since each side is symetrical, use the negative value for the lower bound.
     lower_bound = mean(dataArray) - stats.t.ppf(confidence,len(dataArray)) * (std(dataArray)/sqrt(len(dataArray)))
     upper_bound = mean(dataArray) + stats.t.ppf(confidence,len(dataArray)) * (std(dataArray)/sqrt(len(dataArray)))
-    return [lower_bound, upper_bound]
+    return [lower_bound, upper_bound, confidence]
 
 def confidence_chi(dataArray, confidence = .95):
     """
-    Returns an list with the confidence intervals of the STD.
+    Returns an list with the confidence intervals of the varience.
     Keyword arguments:
     dataArray -- Any list of int or float type
     confidence -- a decimal speciffying the confidence intervals (default - .95)
     """
-    confidence = (1 - confidence) / 2
-    print "Confidence intervals:", confidence
+    confidence = (1 - confidence) / 2 # for a 95% confidence interval, .025 and .975 are the upper and lower limits, for chi, these are asymmetrical
     df = len(dataArray)
     upper_bound = ((df-1) * (std(dataArray)**2)) / (stats.chi2.ppf(confidence, df))
     lower_bound = ((df-1) * (std(dataArray)**2)) / (stats.chi2.ppf(1.0 - confidence,df))
-    return [lower_bound, upper_bound]
+    return [lower_bound, upper_bound, confidence]
     
 def main():
     # Test case
     a = random.sample(xrange(10),6)
     print "The z-scores for", a, "are:", z_score(a)
-    print "Chiquare values:", confidence_chi(a)
-    print "T confidence values:", confidence_t(a)
-    print stats.chi.ppf(.975,9)
-    print
-    print confidence_chi(a,.8)
+    print "Chi-square confidence values:", confidence_chi(a)[0], ":", confidence_chi(a)[1]
+    print "T confidence values:", confidence_t(a)[0], ":", confidence_t(a)[1]
+
 if __name__ == "__main__":
     main()
